024eb51078316395cac6b97ae760e46775b09df7
DavidGomes <74707345+DavidGomes007@users.noreply.github.com>
2025-02-05 20:05:31 -0300

    c

---
+++ Estudos02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+  char opçao = 's';
+  float peso = 1.90;
+  int idade = 23 ;
+  char nome[20] = " David " ;
+
+printf (" Idade  %d \n",idade );
+printf( " opçao  %c \n", opçao);
+printf(" Peso  %f \n" , peso);
+printf(" Idade  %d \n " , idade) ; 
+printf ("Nome %s \n" , nome ) ;
+
+
+}
